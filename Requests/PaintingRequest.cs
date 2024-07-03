6658f6b8a053774036f6820a04585c741394f820
Shatantra Maji <55249027+x3shat@users.noreply.github.com>
2024-07-03 20:56:24 +0530

    Create PaintingRequest.cs

---
+++ Requests/PaintingRequest.cs
@@ -0,0 +1,9 @@
+namespace TaskService
+{
+    public class PaintingRequest
+    {
+        public string Color { get; set; }
+        public string Type { get; set; }
+        public string Note { get; set; }
+    }
+}
